f99f3ee4670c6ce99f13447ad3234d7549215ff7
pangyouzhen <pangyouzhen@live.com>
2020-02-23 22:42:23 +0800

    add OrderDetailRespositoryTest

--- order/src/test/java/com/imooc/order/respository/OrderDetailRespositoryTest.java
+++ order/src/test/java/com/imooc/order/respository/OrderDetailRespositoryTest.java
@@ -1,34 +1,35 @@
 package com.imooc.order.respository;
 
 import com.imooc.order.OrderApplicationTests;
-import com.imooc.order.entity.OrderMaster;
-import com.imooc.order.enums.OrderStatusEnum;
-import com.imooc.order.enums.PayStatusEnum;
+import com.imooc.order.entity.OrderDetail;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Component
 public class OrderDetailRespositoryTest extends OrderApplicationTests {
     @Autowired
     private OrderDetailRespository orderDetailRespository;
 
-//    @Test
-//    public void testSave()  {
-//        OrderMaster orderMaster = new OrderMaster();
-//        orderMaster.setBuyerAddress("慕课网");
-//        orderMaster.setBuyerName("pyz");
-//        orderMaster.setBuyerOpenid("123456");
-//        orderMaster.setBuyerPhone("123456");
-//        orderMaster.setOrderAmount(new BigDecimal(2.5));
-//        orderMaster.setOrderId("123456");
-//        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
-//        orderMaster.setPayStatus(PayStatusEnum.NEW.getCode());
-//
-//        OrderMaster orderMaster1 = orderDetailRespository.save(orderMaster);
-//        Assert.assertTrue(orderMaster1 != null);
-//    }
+    @Test
+    public void testSave() {
+        OrderDetail orderDetail = new OrderDetail();
+        orderDetail.setCreateTime(new Date());
+        orderDetail.setUpdateTime(new Date());
+        orderDetail.setDetailId("123456");
+        orderDetail.setOrderId("123456");
+        orderDetail.setProductIcon("");
+        orderDetail.setProductId("10");
+        orderDetail.setProductName("aaaa");
+        orderDetail.setProductPrice(new BigDecimal(4.5));
+        orderDetail.setProductQuantity(3);
+
+
+        OrderDetail save = orderDetailRespository.save(orderDetail);
+        Assert.assertNotNull(save);
+    }
 }
